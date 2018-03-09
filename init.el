8563483286b7a04311217403017f3e4b47ce1b05
Dawid 'daedreth' Eckert <eckertdawid@gmail.com>
2018-03-09 01:32:50 +0000

    fixed safe theme

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;;; This fixed garbage collection, makes emacs start up faster
+(setq-default gc-cons-threshold 100000000)
+
 ;;; This is all kind of necessary
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -10,14 +13,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
-(when (file-readable-p "~/.emacs.d/config.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-
-;;; Experimental email stuff.
-(when (file-readable-p "~/.email/email.org")
-  (org-babel-load-file (expand-file-name "~/.email/email.org")))
-
 ;;; Anything below is personal preference
 ;;; You can change the font to suit your liking, it won't break anything.
 ;;; The one currently set up is called Terminus
@@ -28,6 +23,10 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(custom-enabled-themes (quote (zerodark)))
+ '(custom-safe-themes
+   (quote
+    ("bce3ae31774e626dce97ed6d7781b4c147c990e48a35baedf67e185ebc544a56" "0e6e456b15dbeb6e7bcad4131f029e027cceecc3cf1598fc49141343860bfce6" default)))
  '(package-selected-packages
    (quote
     (twittering-mode zerodark-theme pretty-mode flycheck-clang-analyzer flycheck-irony flycheck yasnippet-snippets yasnippet company-c-headers company-shell company-irony irony irony-mode company-lua mark-multiple expand-region swiper popup-kill-ring dmenu ido-vertical-mode ido-vertical ox-html5slide centered-window-mode htmlize ox-twbs diminish erc-hl-nicks symon rainbow-mode switch-window dashboard smex company sudo-edit emms magit org-bullets hungry-delete beacon linum-relative spaceline fancy-battery exwm which-key use-package)))
@@ -40,3 +39,11 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :foundry "1ASC" :family "xos4 Terminus"))))
  '(fringe ((t (:background "#292b2e")))))
+
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+;;; Experimental email stuff.
+(when (file-readable-p "~/.email/email.org")
+  (org-babel-load-file (expand-file-name "~/.email/email.org")))
